0190aa1cb0c6856270e09bfeada0483f46b90a7f
Solomio Sisante <solomiosisante@gmail.com>
2024-11-05 03:16:40 -0800

    added code to make createenv.sh file executable

--- initializebashfiles.sh
+++ initializebashfiles.sh
@@ -6,7 +6,7 @@ LOG_FILE="/workspaces/nextjs-dashboard/initializebashfiles.log"
 
   chmod +x /workspaces/nextjs-dashboard/setup.sh
   #chmod +x /workspaces/nextjs-dashboard/initapps.sh
-  
+  chmod +x /workspaces/nextjs-dashboard/createenv.sh
   ./setup.sh
 
   echo "initializebashfiles completed"
