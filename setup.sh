53154e0a41ca864e470bd92e57545747eca5fa35
Solomio Sisante <solomiosisante@gmail.com>
2024-11-05 17:05:43 -0800

    specified the vercel version

--- setup.sh
+++ setup.sh
@@ -48,8 +48,8 @@ LOG_FILE="/workspaces/nextjs-dashboard/setup.log"
     echo "Installing node modules..."
     pnpm i
 
-    echo "Installing vercel cli..."
-    yarn global add vercel@latest 
+    echo "Installing vercel cli version 37.14.0..."
+    yarn global add vercel@37.14.0 
 
     # Add Vercel to the PATH
     export PATH="$PATH:/home/codespace/.yarn/bin/"
